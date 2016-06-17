cb473fee50b0f83a6ebf96757c66bb35b00230b1
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-06-17 16:20:58 +0000

    Switched to using timeout.com website and created comments for css selectors to use

--- lib/met_curr_exhibits/scraper.rb
+++ lib/met_curr_exhibits/scraper.rb
@@ -4,18 +4,18 @@ require 'open-uri'
 
 class Scraper
   def get_page
-    Nokogiri::HTML(open("http://www.metmuseum.org/exhibitions/current-exhibitions"))
+    Nokogiri::HTML(open("http://www.timeout.com/newyork/art/the-best-free-art-exhibitions-in-nyc"))
   end
   
   def scrape_index_page
-    page =get_page.css("div.grid-listing__item")
+    page = get_page.css("article.feature-item")
     binding.pry
   end
   
   #First, iterate through the #scrape_index_page method
-  #link: card.css("a.card--exhibit").attr("href").value  PLAY AROUND WITH THIS IN PRY
-  #title and subtitle: card.css("h2.card__title").text
-  #date: card.css(".card__meta strong")
+  #link for venue details: "http://www.timeout.com" + card.css("a.read-more").attr("href").value + "#tab_panel_2" 
+  #title and subtitle: card.css("h3 a").text
+  #description: card.css("p.feature_item__annotation--truncated")
 end
 
 Scraper.new.scrape_index_page
